5e648f9f1959c794563c5515c2d71e7ff8353134
m.mohamadian <mohammadianmx@gmail.com>
2023-11-04 18:26:22 +0330

    creating the endpoint

--- src/main/java/io/mxm/GreetingResource.java
+++ src/main/java/io/mxm/GreetingResource.java
@@ -5,12 +5,21 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
+    @Path("/h")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        return "Hello RESTEasy";
+    }
+
+
+    @GET
+    @Path("/h2")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello2() {
         return "Hello RESTEasy";
     }
 }
